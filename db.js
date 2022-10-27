c37e04e226981fbb0d4d43fc9cba1ec3afc0e62a
Ishwardeep Anand <ishwardeepanand@Ishwardeeps-MacBook-Pro.local>
2022-10-26 18:25:05 -0700

    created tables with schema

--- db.js
+++ db.js
@@ -1,6 +1,24 @@
-const viewEmployees = ('viewEmployees')
-const viewDepartments = ('viewDepartments')
-const viewRoles = ('viewRoles')
-const addDepartment = ('addDepartment')
-const addRole = ('addRole')
-const addEmployee = ('addEmployee')
+const db = require("./connection")
+class DB {
+    constructor(connection) { this.connection = connection }
+
+    viewEmployees = () => {
+        return this.connection.promise().query("select * from employee")
+
+    }
+   
+    viewDepartments = () => {
+        return this.connection.promise().query("select * from department")
+
+    }
+   
+    addDepartment = () => {
+        return this.connection.promise().query("insert into department set name = ? ")
+
+    }
+
+}
+
+
+
+module.exports = new DB(db)
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const inquirer = require("inquirer")
-const data = require('./db')
+const db = require("./db")
 const mainQuestions = [
     {
         type: 'list',
         name: 'option',
         message: 'What do you want to do?',
-        choice: [
+        choices: [
             { name: "view all employess", value: "VIEW_EMPLOYEES" },
-            { name: "view all departments", value: "VIEW_DEPRATMENTS" },
+            { name: "view all departments", value: "VIEW_DEPARTMENTS" },
             { name: "view all roles", value: "VIEW_ROLES" },
             { name: "add a department", value: "ADD_DEPARTMENT" },
             { name: "add a role", value: "ADD_ROLE" },
@@ -19,6 +19,88 @@ const mainQuestions = [
     }]
 
 function main() {
-    inquirer.prompt(mainQuestions).then(answers => { console.log(answers); })
+    inquirer.prompt(mainQuestions).then(answers => {
+
+        switch (answers.option) {
+
+            case "VIEW_EMPLOYEES":
+                viewEmployees()
+                break
+            case "VIEW_DEPARTMENTS":
+                viewDepartments()
+                break
+            case "VIEW_ROLES":
+                viewRoles()
+                break
+            case "ADD_DEPARTMENTS":
+                addDepartment()
+            case "ADD_ROLE":
+                addRole()
+            case "ADD_EMPLOYEE":
+                addEMPLOYEE()
+        }
+
+
+    })
 }
 main()
+
+const viewEmployees = () => {
+
+    db.viewEmployees().then(([rows]) => {
+
+        console.table(rows)
+
+    }).then(() => { main() })
+
+}
+const viewDepartments = () => {
+
+    db.viewDepartments().then(([rows]) => {
+
+        console.table(rows)
+
+
+    }).then(() => { main() })
+
+}
+const viewRoles = () => {
+
+    db.viewRoles().then(([rows]) => {
+
+        console.table(rows)
+
+
+    }).then(() => { main() })
+
+
+
+}
+
+const addDepartment = () => {
+
+    db.addDepartment().then(([rows]) => {
+
+        console.table(rows)
+
+
+    }).then(() => { main() })
+}
+const addRole = () => {
+
+    db.addRole().then(([rows]) => {
+
+        console.table(rows)
+
+
+    }).then(() => { main() })
+}
+const addEmployee = () => {
+
+    db.addEmployee().then(([rows]) => {
+
+        console.table(rows)
+
+
+    }).then(() => { main() })
+}
